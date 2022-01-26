531b28c0a97a259f7a79a4f1b2125f6ef08eea22
Hadis Sharif <hadis.sharif@gmail.com>
2022-01-26 22:59:25 +0330

    @env added to  googleConfig

--- src/app/google/googleConfig.ts
+++ src/app/google/googleConfig.ts
@@ -1,9 +1,10 @@
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from "@env";
 
 
 export const googleSignInConfig = {
     scopes: ['email'],
-    iosClientId:IOS_CLIENT_ID,
-    webClientId:WEB_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    webClientId: WEB_CLIENT_ID,
     offlineAccess: true,
 }
 
