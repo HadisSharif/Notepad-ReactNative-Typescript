479f63da95d8fb7de212f90f414f88ebb694ff0f
Hadis Sharif <hadis.sharif@gmail.com>
2022-01-26 22:58:34 +0330

    env varible added

---
+++ src/app/types/env.d.ts
@@ -0,0 +1,10 @@
+declare module '@env' {
+    export const CLIENT_ID: string;
+    export const API_KEY: string;
+    export const DATABSE_URL: string;
+    export const APP_ID: string;
+    export const STORAGE_BUCKET: string;
+    export const MESSAGING_SENDER_ID: string;
+    export const IOS_CLIENT_ID: string;
+    export const WEB_CLIENT_ID: string;
+}
