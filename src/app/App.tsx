772d49e901b82c46fb6e93eb509c3f73373904a5
Hadis Sharif <hadis.sharif@gmail.com>
2022-02-03 19:48:04 +0330

    App class changed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {MainNavigation} from './navigation/MainNavigation';
+import {Index} from './navigation';
 import {navigationRef} from './navigation/Navigate';
 
 const App = () => {
   return (
     <NavigationContainer ref={navigationRef}>
-      <MainNavigation />
+      <Index />
     </NavigationContainer>
   );
 };
