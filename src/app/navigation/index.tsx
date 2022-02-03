e10a59c4563ba4975230463e8eaeb2c5ac7ce1d6
Hadis Sharif <hadis.sharif@gmail.com>
2022-02-03 19:48:29 +0330

    index class changed

--- src/app/navigation/index.tsx
+++ src/app/navigation/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import SingleNotePage from '../../features/note/SingleNote';
-import ProfilePage from '../../features/googleSignIn/Profile';
-import SignInPage from '../../features/googleSignIn/SignIn';
-import {createStackNavigator} from '@react-navigation/stack';
-import {useSelector} from 'react-redux';
-import {RootState} from '../store';
-import {MainBtmTabs} from './MainBottomTabs';
-import {HeaderBackBtn, ScreenOptions} from '../../common/NavigationUI';
-import {Strings} from '../../common/utils/Strings';
+import React from "react";
+import SingleNotePage from "../../features/note/SingleNote";
+import ProfilePage from "../../features/googleSignIn/Profile";
+import SignInPage from "../../features/googleSignIn/SignIn";
+import SplashPage from "../../features/Splash";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import { MainBtmTabs } from "./MainBottomTabs";
+import { HeaderBackBtn, ScreenOptions } from "../../common/NavigationUI";
+import { Strings } from "../../common/utils/Strings";
 
 type RootStackParamList = {
   SignInPage: undefined;
   MainBottomTabs: undefined;
   SingleNotePage: undefined;
   ProfilePage: undefined;
+  SplashPage: undefined;
 };
 const RootStack = createStackNavigator<RootStackParamList>();
 
@@ -21,26 +23,33 @@ export const Index = () => {
   const currentTheme = useSelector((state: RootState) => state.theme.themeType);
 
   const userGoogleData = useSelector(
-    (state: RootState) => state.userGoogleData.items,
+    (state: RootState) => state.userGoogleData.items
   );
   let isUserEmpty = Object.values(userGoogleData).every(
-    x => x === null || x === '',
+    (x) => x === null || x === ""
   );
 
   return (
     <RootStack.Navigator>
       {isUserEmpty ? (
-        <RootStack.Screen
-          options={{headerShown: false}}
-          name="SignInPage"
-          component={SignInPage}
-        />
+        <>
+          <RootStack.Screen
+            options={{ headerShown: false }}
+            name="SplashPage"
+            component={SplashPage}
+          />
+  <RootStack.Screen
+            options={{ headerShown: false }}
+            name="SignInPage"
+            component={SignInPage}
+          />
+        </>
       ) : (
         <>
           <RootStack.Screen
             name="MainBottomTabs"
             component={MainBtmTabs}
-            options={{headerShown: false}}
+            options={{ headerShown: false }}
           />
           <RootStack.Screen
             name="SingleNotePage"
